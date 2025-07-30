6b7449ef5d9359097dffd1fcc61f40560dc5fee4
Veda-OuO <gregwreese@gmail.com>
2025-07-30 17:58:00 -0400

    fix2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://Veda-OuO.github.io',
   base: '/Astro-withthecandlestickmysteries',
-    build: {
-    assets: 'assets'
+  build: {
+    assets: '_astro'
+  },
+  vite: {
+    define: {
+      __BASE_URL__: '"/Astro-withthecandlestickmysteries/"'
+    }
   }
 });
